7c899199c876bc349a19c9b6715f370e2f78d313
ETHNOTECH <ETHNOTECH@DESKTOP-J534CID>
2025-07-24 02:27:39 -0700

    Assignment2

---
+++ C2TCassignment/src/assignment2/Product.java
@@ -0,0 +1,23 @@
+package assignment2;
+
+public class Product {
+	 String productName;
+	    int productId;
+	    double price;
+
+	    // Constructor to initialize product details
+	    public Product(String productName, int productId, double price) {
+	        this.productName = productName;
+	        this.productId = productId;
+	        this.price = price;
+	    }
+
+	    // Method to display product details
+	    public void display() {
+	        System.out.println("Product Name: " + productName);
+	        System.out.println("Product ID: " + productId);
+	        System.out.println("Price: $" + price);
+	        System.out.println("----------------------");
+	    }
+}
+
