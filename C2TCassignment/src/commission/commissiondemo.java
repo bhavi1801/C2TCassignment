5bfe43bc2f3d2f931c9a7d15f78985ade9e32467
ETHNOTECH <ETHNOTECH@DESKTOP-J534CID>
2025-07-24 02:55:10 -0700

    assignment

---
+++ C2TCassignment/src/commission/commissiondemo.java
@@ -0,0 +1,21 @@
+package commission;
+
+public class commissiondemo {
+
+
+	public static void main(String[] args) {
+		commission c=new commission();
+		c.setName("bhava");
+		c.setAddress("usha street, ariyur");
+		c.setPhone("6380419085");
+		c.setSalesAmount(80000);
+		
+		c.SaleCommission();
+		
+		
+		
+	}
+
+}
+
+
